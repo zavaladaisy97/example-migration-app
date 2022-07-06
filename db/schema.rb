f0a47e1c77913e10fd2379f05bff81afb8dc613c
daisyalexandria <dazavala7@gmail.com>
2022-07-05 19:31:06 -0500

    created a standalone migration to add a price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_231857) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_002648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_231857) do
     t.string "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
   end
 
 end
